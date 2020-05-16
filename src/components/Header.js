67755d7c16a7f218ca7bd48b119f75974884872a
赤堀将太郎 <akahorisusumutarou@MacBook-Air.local>
2020-05-16 21:14:59 +0900

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Footer from './Footer'
+import avatar from '../assets/images/avatar.jpg'
+
+const Header = () => (
+  <header id="header">
+    <div className="inner">
+      <a href="#" className="image avatar">
+        <img src={avatar} alt="" />
+      </a>
+      <h1>
+        <strong>赤堀将太郎</strong>, ポートフォリオサイトです
+        <br />
+        React.jsを駆使して作成したポートフォリオサイトです
+        <br />
+        crafted by <a href="http://html5up.net">HTML5 UP</a>.
+      </h1>
+    </div>
+    <Footer />
+  </header>
+)
+
+export default Header
